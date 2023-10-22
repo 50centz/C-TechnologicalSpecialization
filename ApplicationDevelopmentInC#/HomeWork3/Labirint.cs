cdf88e63e506cd2a3c57a56dc937d31bc6341873
Федоров Евгений Дом ПК <zen-g@mail.ru>
2023-10-22 12:24:07 +0300

    Third homework complete

---
+++ ApplicationDevelopmentInC#/HomeWork3/Labirint.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HomeWork3
+{
+    internal class Labirint
+    {
+        
+        public static int HasExit(int startI, int startJ, int[,] l)
+        {
+            int count = 0;
+
+            if (l[startI, startJ] == 1)
+            {
+                Console.WriteLine("Начали с стены");
+                return count;
+            }
+
+            if (l[startI, startJ] == 2)
+            {
+                count = +1;
+            }
+
+            Stack<Tuple<int, int>> stack = new Stack<Tuple<int, int>>();
+
+            stack.Push(new Tuple<int, int>(startJ, startI));
+
+            while (stack.Count > 0)
+            {
+                var tmp = stack.Pop();
+
+                if (l[tmp.Item1, tmp.Item2] == 2)
+                {
+                    count++;
+                }
+
+                l[tmp.Item1, tmp.Item2] = 1;
+
+                if (tmp.Item2 == 0 && l[tmp.Item1 - 1, tmp.Item2] != 1)
+                    stack.Push(new Tuple<int, int>(tmp.Item1 - 1, tmp.Item2));
+                if (tmp.Item2 == 0 && l[tmp.Item1 + 1, tmp.Item2] != 1)
+                    stack.Push(new Tuple<int, int>(tmp.Item1 + 1, tmp.Item2));
+                if (tmp.Item2 == 0 && l[tmp.Item1, tmp.Item2 + 1] != 1)
+                    stack.Push(new Tuple<int, int>(tmp.Item1, tmp.Item2 + 1));
+
+
+                if (tmp.Item2 > 0 && tmp.Item2 + 1 < l.GetLength(1) && l[tmp.Item1, tmp.Item2 + 1] != 1)
+                    stack.Push(new Tuple<int, int>(tmp.Item1, tmp.Item2 + 1));
+                if (tmp.Item2 > 0 && tmp.Item1 - 1 > 0 && l[tmp.Item1 - 1, tmp.Item2] != 1)
+                    stack.Push(new Tuple<int, int>(tmp.Item1 - 1, tmp.Item2));
+                if (tmp.Item2 > 0 && tmp.Item1 + 1 < l.GetLength(0) && l[tmp.Item1 + 1, tmp.Item2] != 1)
+                    stack.Push(new Tuple<int, int>(tmp.Item1 + 1, tmp.Item2));
+
+            }
+
+
+            return count;
+        }
+    }
+}
---
+++ ApplicationDevelopmentInC#/HomeWork3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] l = new int[,]
+            {
+            {1, 1, 1, 1, 1, 1, 1 },
+            {1, 0, 0, 0, 0, 0, 1 },
+            {1, 0, 1, 1, 1, 0, 1 },
+            {0, 0, 0, 0, 1, 0, 2 },
+            {1, 1, 0, 0, 1, 1, 1 },
+            {1, 1, 1, 0, 1, 1, 1 },
+            {1, 1, 1, 2, 1, 1, 1 }
+            };
+
+            Console.WriteLine(Labirint.HasExit(1, 1, l));
+            Console.ReadKey();
+        }
+    }
+}
