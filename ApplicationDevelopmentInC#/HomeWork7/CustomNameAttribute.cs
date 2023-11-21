45b8dd4fcda06ba8e07b9e5539b810ddd9ef6162
Федоров Евгений Дом ПК <zen-g@mail.ru>
2023-11-21 21:24:31 +0300

    Seventh homework complete

---
+++ ApplicationDevelopmentInC#/HomeWork7/CustomNameAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork7_1
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    internal class CustomNameAttribute : Attribute
+    {
+        public string CustomFieldName { get; }
+
+        public CustomNameAttribute(string customFieldName)
+        {
+            CustomFieldName = customFieldName;
+        }
+    }
+}
