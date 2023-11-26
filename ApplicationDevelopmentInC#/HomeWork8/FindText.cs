1a3e1caa60a78923727c498ddb2cd21de8e52d6e
Федоров Евгений Дом ПК <zen-g@mail.ru>
2023-11-26 17:08:29 +0300

    Eighth homework completed

---
+++ ApplicationDevelopmentInC#/HomeWork8/FindText.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork8
+{
+    internal class FindText
+    {
+        public void findTextFromFile(string filePath, string extension, string text)
+        {
+         
+            var dirs = Directory.EnumerateDirectories(filePath);
+            string[] files = Directory.GetFiles(filePath, extension);
+
+            foreach (var dir in dirs)
+            {
+                findTextFromFile(dir, extension, text);
+                foreach (var file in files)
+                {
+                    using (StreamReader sr = new StreamReader(file))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string? value = sr.ReadLine();
+                            if (value.Contains(text))
+                            {
+                                Console.WriteLine(file);
+                                Console.WriteLine(value);
+                                Console.WriteLine();
+                            }
+                        }
+                    }
+                }
+
+            }
+
+
+        }
+    }
+}
---
+++ ApplicationDevelopmentInC#/HomeWork8/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace HomeWork8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:\\Users\\admin\\Desktop\\C#TechnologicalSpecialization\\ApplicationDevelopmentInC#\\HomeWorks";
+            string extension = "*.txt";
+            string text = "Hello";
+
+            FindText findText = new FindText();
+            //findText.findTextFromFile(path, extension, text);
+            findText.findTextFromFile(args[0], args[1], args[2]);
+
+        }
+    }
+}
