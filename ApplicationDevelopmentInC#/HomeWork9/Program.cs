d083aec4a28269a4f306bd056e53c4769833de89
Федоров Евгений Дом ПК <zen-g@mail.ru>
2023-11-30 22:06:02 +0300

    Ninth homework complete

---
+++ ApplicationDevelopmentInC#/HomeWork9/Program.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System.Data.Common;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace HomeWork9_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string json = "{" +
+                    "\"Current\":" +
+                    "{\"Time\":" +
+                        "\"2023-06-18T20:35:06.722127+04:00\",\"Temperature\":29,\"Weathercode\":1,\"Windspeed\":2.1,\"Winddirection\":1}," +
+                    "\"History\":" +
+                        "[" +
+                            "{\"Time\":\"2023-06-17T20:35:06.77707+04:00\",\"Temperature\":29,\"Weathercode\":2,\"Windspeed\":2.4,\"Winddirection\":1}," +
+                            "{\"Time\":\"2023-06-16T20:35:06.777081+04:00\",\"Temperature\":22,\"Weathercode\":2,\"Windspeed\":2.4,\"Winddirection\":1}," +
+                            "{\"Time\":\"2023-06-15T20:35:06.777082+04:00\",\"Temperature\":21,\"Weathercode\":4,\"Windspeed\":2.2,\"Winddirection\":1}" +
+                        "]" +
+                        "}";
+           
+
+            XDocument xml = XDocument.Load(JsonReaderWriterFactory.CreateJsonReader(Encoding.ASCII.GetBytes(json), new XmlDictionaryReaderQuotas()));
+            xml.Save("out.xml");
+            
+        }
+    }
+}
