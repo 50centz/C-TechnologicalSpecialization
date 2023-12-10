d9b5ea838898410a7e6ae387f863250dd6fc6a41
Федоров Евгений Дом ПК <zen-g@mail.ru>
2023-12-10 16:50:12 +0300

    Third homework complete

---
+++ DevelopmentOfANetworkApplication/HomeWork3/Client/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace Client
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await Task.Run(() => Client());
+        }
+
+
+        public static async void Client()
+        {
+
+
+            UdpClient client = new UdpClient();
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 44444);
+
+
+            string messageText;
+
+            while (true)
+            {
+
+
+                do
+                {
+                    Console.Clear();
+                    Console.WriteLine("Enter message ");
+                    messageText = Console.ReadLine();
+
+                } while (string.IsNullOrEmpty(messageText));
+
+                byte[] data = Encoding.UTF8.GetBytes(messageText);
+
+                client.Send(data, data.Length, endPoint);
+
+                Thread.Sleep(2000);
+            }
+        }
+
+    }
+}
---
+++ DevelopmentOfANetworkApplication/HomeWork3/Server/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace HomeWork3
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await Task.Run(() => Server());
+        }
+
+        public static async void Server()
+        {
+            UdpClient udp = new UdpClient(44444);
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            CancellationToken ct = cts.Token;
+
+            Console.WriteLine("We are waiting for the connection ");
+
+            while (!ct.IsCancellationRequested)
+            {
+                byte[] buffer = udp.Receive(ref iPEndPoint);
+
+                var messageText = Encoding.UTF8.GetString(buffer);
+
+                if (messageText != null)
+                {
+                    if (messageText.Equals("Exit"))
+                    {
+                        Console.WriteLine("The server has shut down");
+                        cts.Cancel();
+                    }
+                    Console.WriteLine(messageText);
+
+                }
+            }
+        }
+    }
+}
