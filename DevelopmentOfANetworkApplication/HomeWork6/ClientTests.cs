1297e055b2053f0d161cae26a3f13ccd05c03c4e
Федоров Евгений Дом ПК <zen-g@mail.ru>
2023-12-21 06:46:11 +0300

    Sixth homework complete

---
+++ DevelopmentOfANetworkApplication/HomeWork6/ClientTests.cs
@@ -0,0 +1,47 @@
+﻿using Moq;
+using Seminar5;
+using Seminar5.Abstracts;
+using Seminar5.Services;
+using Seminar5.Model;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ClientTests
+{
+    public class ClientTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+
+        }
+
+        [Test]
+        public void RegisterTest()
+        {
+            var name = "ClientTests";
+            var address = "127.0.0.1";
+            var port = 4444;
+
+            var messageSouce = new Mock<IMessageSouce>();
+            var udpClient = new Mock<UdpClient>();
+
+            Client client = new Client(name, address, port)
+            {
+                _messageSouce = messageSouce.Object,
+                udpClientClient = udpClient.Object
+            };
+
+            var remoteEndPoint = new IPEndPoint(IPAddress.Any, 9999);
+
+
+            client.Register(remoteEndPoint);
+
+            
+        }
+
+       
+
+    }
+}
