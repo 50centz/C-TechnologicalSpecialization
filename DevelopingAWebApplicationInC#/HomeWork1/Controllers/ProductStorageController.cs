6bf4c63e69855edc8993b7ef316598eb1a010179
Федоров Евгений Дом ПК <zen-g@mail.ru>
2024-01-12 21:19:52 +0300

    First HomeWork complete

---
+++ DevelopingAWebApplicationInC#/HomeWork1/Controllers/ProductStorageController.cs
@@ -0,0 +1,32 @@
+﻿using HomeWork1.DB;
+using HomeWork1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeWork1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductStorageController : ControllerBase
+    {
+        [HttpGet(template: "GetProductInStorage")]
+        public IActionResult GetProductInStorage(string StorageName)
+        {
+            try
+            {
+                using (var context = new ProductContext())
+                {
+                    var storage = context.Storages.FirstOrDefault(e => e.Name.Equals(StorageName));
+                    int sstorageId = storage.Id;
+                    
+
+                    return Ok(context.ProductStorages.FirstOrDefault(e => e.StorageId == sstorageId));
+
+                }
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
